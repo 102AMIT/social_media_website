b6cdf27ccd5345c252f2c3c7c538e155906a1bf4
amit <kingston.amit9830@gmail.com>
2022-09-02 03:14:07 +0530

    we are just making a display posts to related user and aslo some authentication applied if the user not sign in then he not able to do post and also from html of developer tool they can't do a post via developer tool

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,4 @@
+const Post = require('../models/post');
 module.exports.home=function(req,res){
     // here we get the cookies from brower we need to set first 
     // first we need to go to console then application then cookies then the expend it click on url and set the name as id and value according to that id 
@@ -10,9 +11,25 @@ module.exports.home=function(req,res){
     // this is change via response as you can see below by providing id and value 
     // res.cookie('user_id',25);//repalce the value for specific id
 
-    return res.render('home',{
-        title:"Home"
+    
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title:"Codial | Home",
+    //         posts : posts
+    //     });
+    // });
+
+    // here we are using populate populate is use for populating data we want populate is mongoose property
+    // exec is a call back function
+    // populate the user for each post
+    // after this we got the whole user obaject we want to display the name with the help of ejs
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title:"Codial | Home",
+            posts : posts
+        });
     })
+    
 
     //this is diretly showing to the brower
     // return res.end('<h1>Express is up for codieal</h1>');
--- routes/posts.js
+++ routes/posts.js
@@ -4,10 +4,12 @@ const express=require('express');
 //this router const are within this module
 const router=express.Router();
 
-
+// here we need to import the passport
+const passport=require('passport');
 const postsController=require('../controllers/posts_controller');
-
-router.post('/create',postsController.create);
+// only the user posting the post we sign in that why we are using passport authentication
+// we already cretated passport .checkAuthentication in config folder in passport-local-strategy
+router.post('/create',passport.checkAuthentication,postsController.create);
 
 
 module.exports=router;
