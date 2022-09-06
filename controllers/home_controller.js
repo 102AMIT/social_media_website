cbf9cbaf84ab9e451e1272d12defd5cbcb06924f
amit <kingston.amit9830@gmail.com>
2022-09-07 02:28:01 +0530

    we are just complete converting to async await of home_controller now we convert more file on async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,51 +2,37 @@ const Post = require('../models/post');
 const User=require('../models/user');
 
 
-module.exports.home=function(req,res){
-    // here we get the cookies from brower we need to set first 
-    // first we need to go to console then application then cookies then the expend it click on url and set the name as id and value according to that id 
-    // then we console here so name or id and value are printed when we refresh the browser according to that we get the value and id here 
-    // cookies coming with req and going back in res 
-    
-    // console.log(req.cookies);
-
-    // here we change the cookies
-    // this is change via response as you can see below by providing id and value 
-    // res.cookie('user_id',25);//repalce the value for specific id
-
-    
-    // Post.find({},function(err,posts){
-    //     return res.render('home',{
-    //         title:"Codial | Home",
-    //         posts : posts
-    //     });
-    // });
+module.exports.home=async function(req,res){
 
     // here we are using populate populate is use for populating data we want populate is mongoose property
     // exec is a call back function
     // populate the user for each post
     // after this we got the whole user obaject we want to display the name with the help of ejs
-    Post.find({}).populate('user')
-    // ************************************* explaination needed
-    .populate({
-        path:'comments',
-        populate:{
-            path:'user'
-        }
-    })
-    .exec(function(err,posts){
 
-        User.find({},function(err,users){
+    try{
+
+        let posts= await Post.find({}).populate('user')
+    // ************************************* explaination needed
+        .populate({
+            path:'comments',
+            populate:{
+                path:'user'
+            }
+        });
+
+        let users= await User.find({});
+
         return res.render('home',{
             title:"Codial | Home",
             posts : posts,
             all_users:users
         });
-        });
-    })
 
-    
-
-    //this is diretly showing to the brower
-    // return res.end('<h1>Express is up for codieal</h1>');
+        
+    }
+    catch(err){
+        console.log('Error',err);
+        return;
+    }
 }
+    
