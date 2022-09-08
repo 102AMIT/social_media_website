e5cf97966897e874c7984fe71946b2259ce8e291
amit <kingston.amit9830@gmail.com>
2022-09-08 15:38:28 +0530

    i just completed a flash messages setup for different area and now i move to converting my code to ajax

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -13,20 +13,23 @@ const User=require('../models/user');
 // authentication using passport
 passport.use(new LocalStrategy({
     // usernameField is syntax are here we are pass email because email is unique
-        usernameField:'email'
+        usernameField:'email',
+        // this provide us to pass a req as a first argument in function
+        passReqToCallback:true
+        
     },
     // done is inbuilt to passport it's call back function
-    function(email,password,done){
+    function(req,email,password,done){
         // find the user and establish the identity
         // here we need to import user
         // console.log('passport')
         User.findOne({email:email},function(err,user){
             if(err){
-                console.log('Error in finding user--> Passport');
+                req.flash('error',err);
                 return done(err);
             }
             if(!user || user.password != password){
-                console.log('Invalid Username/Password');
+                req.flash('error','Invalid Username/Password');
                 return done(null,false);
             }
             // null means no error
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -7,10 +7,11 @@ module.exports.create=async function(req,res){
         content:req.body.content,
         user:req.user._id
     });
+    req.flash('success','Post Published !');
     return res.redirect('back');
 }catch(err){
-    console.log('Error',err);
-    return;
+    req.flash('error',err);
+    return res.redirect('back');
 }
 
 
@@ -35,14 +36,16 @@ module.exports.destroy=async function(req,res){
             // deleteMany delete all the comment based on query passed
 
             await Comment.deleteMany({post:req.params.id});
+                req.flash('success','Post and associated comments deleted !');
                 return res.redirect('back');
         }else{
             // if user is not found
+            req.flash('error','You cannot delete this post !');
             return res.redirect('back');
         }
 
     }catch{
-        console.log('Error',err);
-        return;
+        req.flash('error',err);
+        return res.redirect('back');
     }  
 }
