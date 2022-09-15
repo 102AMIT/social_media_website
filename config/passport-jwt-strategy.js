ea7c53f0ff94d6c5ac8037e5d25a29c991e781bc
amit <kingston.amit9830@gmail.com>
2022-09-15 09:20:47 +0530

    i just adding a jwt token and now i will implement the code for jwt authentication

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -1,9 +1,9 @@
 const passport=require('passport');
-const User = require('../models/user');
 
 const JWTStrategy=require('passport-jwt').Strategy;
 
 const ExtractJWT=require('passport-jwt').ExtractJwt;
+const User = require('../models/user');
 
 
 let opts={
@@ -13,9 +13,9 @@ let opts={
 
 }
 // for authonticate JWT
-passport.use(new JWTStrategy(opts,function(jwtPayload,done){
+passport.use(new JWTStrategy(opts,function(jwtPayLoad,done){
 
-    User.findById(jwtPayload._id,function(err,user){
+    User.findById(jwtPayLoad._id,function(err,user){
         if(err){console.log('Error finding user from JWT');return;}
 
         if(user){
